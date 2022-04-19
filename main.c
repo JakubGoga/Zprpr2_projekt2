1f5401c3e707a94b662dad64cb8a5d61e2b74d7f
JakubGoga <xgogaj@stuba.sk>
2022-04-19 18:31:31 +0200

    zaciatok projektu, iba switch v maine a osetrenie

--- main.c
+++ main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 
+void n(zaznamy){
+
+}
+
 int main() {
     FILE *subor = NULL;
+    char vstup;
+    int zaznamy;
 
+    do{
+        scanf("%c",&vstup);
+        if(vstup == '\n' || vstup == ' '){
+            scanf("%c",&vstup);
+        }
+    switch (vstup) {
+        case 'n': {
+            n(zaznamy);
+            break;
+        }
+        case 'z': {
+            break;
+        }
+        case 'p': {
+            break;
+        }
+        case 'a': {
+            break;
+        }
+        case 'v': {
+            break;
+        }
+        case 'h': {
+            break;
+        }
+        case 'r': {
+            break;
+        }
 
+        default: printf("Zadali ste nespravny vstup \n");
+            break;
+        }
+    }while(vstup != 'k');
     return 0;
 }
